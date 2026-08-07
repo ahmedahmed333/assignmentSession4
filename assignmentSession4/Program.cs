742798c53392124a1623dd33245fc2dd0f9afbde
Ahmed Ehab <104663084+ahmedehab311@users.noreply.github.com>
2026-08-08 01:32:13 +0300

    Question06

--- assignmentSession4/Program.cs
+++ assignmentSession4/Program.cs
@@ -20,15 +20,24 @@
             //PrintBookTitle("Clean Code");
 
             #endregion
-
-
             #region Question05
-            int pages = 400;
-            AddBonusPages(pages);
+            //int pages = 400;
+            //AddBonusPages(pages);
 
-            Console.WriteLine(pages); //I expect to see 400 because int is a value type, so the method receives a copy of the value.
-                                      //The original pages variable remains unchanged.
+            //Console.WriteLine(pages); //I expect to see 400 because int is a value type, so the method receives a copy of the value.
+            //                          //The original pages variable remains unchanged.
 
+            #endregion
+
+            #region Question06
+
+            double[] prices = { 25.5, 40.0 };
+
+            ApplyDiscount(prices);
+
+            Console.WriteLine(prices[0]); // answer : 20.5 
+                                          // Because double[] is a reference type, so the method modifies the same array,
+                                          // and the change to prices[0] is visible in Main.
             #endregion
         }
         #region Question04
@@ -39,9 +48,15 @@
         //}
         #endregion
         #region Question05
-        static void AddBonusPages(int pages)
+        //static void AddBonusPages(int pages)
+        //{
+        //    pages = pages + 50;
+        //}
+        #endregion
+        #region Question06
+        static void ApplyDiscount(double[] prices)
         {
-            pages = pages + 50;
+            prices[0] = prices[0] - 5;
         }
         #endregion
     }
