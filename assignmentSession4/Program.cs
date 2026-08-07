d8709fbcfa5c012b3e35cf89ea24b95833455879
Ahmed Ehab <104663084+ahmedehab311@users.noreply.github.com>
2026-08-07 23:48:14 +0300

    Question02

--- assignmentSession4/Program.cs
+++ assignmentSession4/Program.cs
@@ -5,8 +5,17 @@
         static void Main(string[] args)
         {
             #region Question01 
-            double[] numbers = { 25.5, 40.0, 33.75 };
-            Console.WriteLine(numbers[1]); // 40
+            //double[] numbers = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(numbers[1]); // 40
+            #endregion
+
+            #region Question02
+            int[,] newArr =
+            {
+                {3,5 },
+                {1,4 }
+            };
+            Console.WriteLine(newArr[1, 0]); // 1
             #endregion
         }
     }
